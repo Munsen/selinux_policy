bc4b70997a3a3d494a67986b8da5be20dfeb6b4d
Brian Cha <brian.cha@retailtrend.com>
2020-03-03 13:27:26 +0900

    Support chef12

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache-2.0'
 description      'Manages SELinux policy components'
 source_url       'https://github.com/sous-chefs/selinux_policy'
 issues_url       'https://github.com/sous-chefs/selinux_policy/issues'
-chef_version     '>= 13.0'
+chef_version     '>= 12.0'
 version          '2.4.0'
 
 supports 'redhat'
